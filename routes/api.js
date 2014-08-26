a25d28277587f5b3f4e339ead4d5a485740afaea
Nick Price <neppers+git@gmail.com>
2014-08-26 11:09:40 +0100

    Remove draft entries from children in JSON responses

--- routes/api.js
+++ routes/api.js
@@ -3,6 +3,13 @@ var router = express.Router();
 var mongoose = require('mongoose');
 var Content = require('../models/content');
 
+var removeDrafts = function(content) {
+    content.children.forEach(function(value, key) {
+        if (value.status === 'draft') content.children.splice(key, 1);
+    });
+    return content;
+};
+
 router.all('*', function(req, res, next) {
     res.locals.currentSection = 'API';
     next();
@@ -16,11 +23,11 @@ router.get('/', function(req, res) {
     }    
 });
 
-router.get('/content/:id', function(req, res) {
+router.get('/content/:id', function(req, res) {    
     if (mongoose.Types.ObjectId.isValid(req.params.id)) {
         Content.findById(req.params.id).populate('children _author').exec(function(err, content) {
             if (!content) res.json({status: 404, message: 'Not found'});
-            res.json(content);
+            res.json(removeDrafts(content));
         });
     } else {
         Content.findOne({
@@ -30,7 +37,7 @@ router.get('/content/:id', function(req, res) {
             Content.populate(content, {
                 path: 'children.children'
             }, function(err, content) {
-                res.json(content);
+                res.json(removeDrafts(content));
             });
         });
     }
