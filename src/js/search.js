d2852894a38383ee4ce569079b1af7622a27cde8
Nick Price <neppers+git@gmail.com>
2014-08-28 15:04:10 +0100

    Search no longer hard-coded to one project

--- src/js/search.js
+++ src/js/search.js
@@ -3,7 +3,8 @@
 var app = angular.module('SearchContent', []);
 
 app.controller('SearchCtrl', ['$scope', '$http', function($scope, $http) {
-    $http.get('api/content/project/53f64accba480a4e192a93f9').success(function(data) {
+    var projectId = angular.element(document.querySelector('[data-id]')).attr('data-id');
+    $http.get('api/content/project/' + projectId).success(function(data) {
         $scope.content = data;
     }).error(function(data) {
 
