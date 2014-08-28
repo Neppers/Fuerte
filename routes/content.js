5daa763aab397cb38660c9ba9cfe8c30d70c1b88
Nick Price <neppers+git@gmail.com>
2014-08-28 13:36:26 +0100

    Added admin only populate route for demo purposes

--- routes/content.js
+++ routes/content.js
@@ -54,6 +54,31 @@ router.get('/add-child/:parent', function(req, res) {
     });
 });
 
+router.get('/populate/:num', function(req, res, next) {
+    if (req.user.role === 'admin') {
+        Project.findById(req.session.project, function (err, project) {
+            if (err) return next(err);
+            if (!project || !req.user._id) return next(new Error(404));
+            for (var i = 0; i < req.params.num; i++) {
+                var content = new Content();
+                content._project = project._id;
+                content._author = req.user._id;
+                content.title = "Dummy content " + i;
+                content.path = "dummy-content-" + i;
+                content.status = 'published';
+                content.body = "The path of the righteous man is beset on all sides by the iniquities of the selfish and the tyranny of evil men. Blessed is he who, in the name of charity and good will, shepherds the weak through the valley of darkness, for he is truly his brother's keeper and the finder of lost children. And I will strike down upon thee with great vengeance and furious anger those who would attempt to poison and destroy My brothers. And you will know My name is the Lord when I lay My vengeance upon thee.";
+                content.save(function (err) {
+                    if (err) return next(err);
+                });
+            }
+            req.flash('success', 'Content populated');
+            res.redirect('/content');
+        });
+    } else {
+        return next(new Error(400));
+    }
+});
+
 /* POST add content */
 router.post('/add', function(req, res, next) {
     Project.findById(req.session.project, function(err, project) {
